42bb63a5d833190ac67f27b5c6f58d05d22ec52f
js1 <jacob@slvr.mn>
2018-12-09 19:52:06 +0200

    comments added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # techdegree-project-5
+
+I did not alter the html or css, only the ./js/scripts.js file. To review the project just open index.html in your
+browser. I went for the exceeds grade. Thanks for your review!
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 let userArr = [];
 
+/* display the 12 random users. this code block prepares the HTML as a string and then replaces the necessary div in
+ the DOM with the new string. This code works for both the first page load and subsequent searches. */
+
 function appendGallery(galleryArray){
   const galleryHTML = [];
   galleryHTML.push('<div id="gallery" class="gallery">');
@@ -12,19 +15,24 @@ alt="profile picture"></div><div class="card-info-container"><h3 id="name" class
   $('div#gallery').replaceWith(galleryHTML.join(''));
 }
 
+/* When any part of an employee item in the directory is clicked, a modal window will pop up, similar to the gallery
+ code this code block works for both the first run and subsequent searches by replacing the modal container in the dom
+  with the filtered results after search */
+
 function appendModal(modalArray){
   const modalHTML = [];
   modalArray.forEach((item,index) => {
     let anotheruser = `<div class="modal-container" id=${item.id} style="display:none"><div class="modal"><button type="button" 
-id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button><div class="modal-info-container"><img class="modal-img" src=${item.image} alt="profile picture"><h3 id="name" class="modal-name cap">${item.name}</h3><p class="modal-text">${item.email}</p><p class="modal-text cap">${item.city}</p><hr><p class="modal-text">${item.cell}</p><p class="modal-text">${item.street}, ${item.city}, ${item.state}, ${item.postcode}</p><p class="modal-text">Birthday: ${item.birth}</p></div></div><div class="modal-btn-container" id=${item.id}>`;
+id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button><div class="modal-info-container"><img class="modal-img" src=${item.image} alt="profile picture"><h3 id="name" class="modal-name cap">${item.name}</h3><p class="modal-text">${item.email}</p><p class="modal-text cap">${item.city}</p><hr><p class="modal-text">${item.cell}</p><p class="modal-text">${item.street}, ${item.city}, ${item.state}, ${item.postcode}</p><p class="modal-text">Birthday: ${item.birth}</p></div></div>`;
     if(index > 0 && index < modalArray.length-1 ){
-      anotheruser = anotheruser + `<button type="button" id="modal-prev" class="modal-prev btn">Prev</button><button type="button" id="modal-next" class="modal-next btn">Next</button></div>`;
+      anotheruser = anotheruser + `<div class="modal-btn-container" id=${item.id}><button type="button" id="modal-prev" class="modal-prev btn">Prev</button><button type="button" id="modal-next" class="modal-next btn">Next</button></div>`;
     } else if (index === 0 && modalArray.length > 1){
-      anotheruser = anotheruser + `<button type="button" id="modal-next" class="modal-next btn">Next</button></div>`;
+      anotheruser = anotheruser + `<div class="modal-btn-container" id=${item.id}><button type="button" id="modal-next" class="modal-next btn">Next</button></div>`;
     } else if (index === modalArray.length-1 && modalArray.length > 1){
-      anotheruser = anotheruser + `<button type="button" id="modal-prev" class="modal-prev btn">Prev</button></div>`;
+      anotheruser = anotheruser + `<div class="modal-btn-container" id=${item.id}><button type="button" id="modal-prev" class="modal-prev btn">Prev</button></div>`;
     } else {
-      anotheruser = anotheruser + '<div />';
+      // there is only 1 result in search
+      anotheruser = anotheruser + '</div>';
     }
     modalHTML.push(anotheruser);
   });
@@ -34,6 +42,10 @@ id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button><div cla
   $('body').append(modalHTML);
 }
 
+/* EXTRA CREDIT */
+
+/* Add a way to filter the directory by name. */
+
 function appendSearch(){
   $('div.search-container').append('<form action="#" method="get">' +
     '<input type="search" id="search-input"' +
@@ -42,15 +54,34 @@ function appendSearch(){
     ' class="search-submit"></form>');
 }
 
-$(document).on('click', '.card',function(e){
+$(document).on('click','.search-submit',function(e){
+  const searchString = $('#search-input').prop('value');
+  userArr = userArr.filter(obj => !obj.name.toLowerCase().indexOf(searchString));
+  if(userArr.length > 0){
+    appendGallery(userArr);
+    appendModal(userArr);
+  } else if (userArr.length===0){
+    $('div#gallery').replaceWith('<div id="gallery" class="gallery"><h3>No results</h3></div>');
+  } else {
+    console.log('should not get here either!')
+  }
+});
+
+/* a user can click anywhere inside the card to open the modal */
+
+ $(document).on('click', '.card',function(e){
   const elId = $(e.target).closest('.card').prop('id');
   $(`.modal-container#${elId}`).show();
 });
+
+/* Make sure there’s a way to close the modal window */
 
 $(document).on('click', '#modal-close-btn',function(e){
   const elId = $(e.target).closest('.modal-container').prop('id');
   $(`.modal-container#${elId}`).hide();
 });
+
+/* Add a way to toggle back and forth between employees when the modal window is open. */
 
 $(document).on('click', '.modal-prev.btn', function(e){
   let currentArrayToNavigate = userArr;
@@ -79,24 +110,29 @@ $(document).on('click', '.modal-next.btn', function(e){
   $(`.modal-container#${nextId}`).show();
 });
 
-$(document).on('click','.search-submit',function(e){
-  const searchString = $('#search-input').prop('value');
-  userArr = userArr.filter(obj => !obj.name.toLowerCase().indexOf(searchString));
-  if(userArr.length > 0){
-    appendGallery(userArr);
-    appendModal(userArr);
-  } else if (userArr.length===0){
-    $('div#gallery').replaceWith('<div id="gallery" class="gallery"><h3>No results</h3></div>');
-  } else {
-    console.log('should not get here either!')
-  }
+/* PERSONAL TOUCH */
+
+/* Add a obvious way to get a new list after filter. */
+
+function appendGetListButton(){
+  $('<button class="reset__button">GET NEW LIST</button>')
+    .insertAfter('.header-text-container')
+    .css({'height':'32px','width':'auto','margin-top':'25px','font-size':'1em'});
+}
+
+$(document).on('click','.reset__button',function(){
+  location.reload();
 });
+
+/* AJAX REQUEST */
 
 $.ajax({
   url: 'https://randomuser.me/api/?inc=picture,name,email,location,nat,dob,cell,id&&results=12&nat=gb,us',
   dataType: 'json',
   success: function(data) {
+    /* PARSE RESPONSE */
     for (const key of Object.keys(data.results)) {
+      /* for each object in the response array, store the required informtion */
       let randUser = {image:'', name:'', email:'', street: '', city: '', state:'', postcode:'', cell: '', birth:'',id:''};
       for (const key2 of Object.keys(data.results[key])) {
         switch (key2) {
@@ -140,8 +176,12 @@ $.ajax({
       }
       userArr.push({image: randUser.image, name: randUser.name, email: randUser.email, street: randUser.street, city: randUser.city, state: randUser.state, postcode: randUser.postcode, cell: randUser.cell, birth: randUser.birth, id: randUser.id});
     }
+    /* call all the helper functions to place the gallery and modal div's, append search bar (extra credit) and get
+     list button (personal touch) */
+
     appendGallery(userArr);
     appendModal(userArr);
     appendSearch();
+    appendGetListButton();
   }
 }).catch(e => console.log('error ',e));
